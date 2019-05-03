ac13761b857c59931c6a1e5b01dca78d148c93cb
Pawel Paradysz <pa.paradysz@gmail.com>
2019-05-03 20:44:23 +0100

    Some tweaking around

--- recommend.py
+++ recommend.py
@@ -17,8 +17,8 @@ apikey = 'd8eaef84c24b7217a5ec54158eacd213'.encode('utf-8')
 if(len(args)<3):
 	print('Usage: recommend.py \'artist 1\' \'song 1\' [\'artist 2\' \'song 2\' ...]\nwhere artist1 song1 is the last track listened to\nAnd the remaining ones are recommendations to choose from.\nIf only first pair is given it matches against songs already in the database.')
 assert(len(args)>2)
-listened_to = args[1], args[2]
 if len(args)==3:
+	listened_to = args[1], args[2]
 	lyr, trid = find(listened_to[0], listened_to[1])
 	lyr=' '.join(lyr.replace('\n', ' ').split(' ')[:-12])
 	elmoisedlyr = elmoiser([lyr])
@@ -26,16 +26,49 @@ if len(args)==3:
 	lyrArr = svd.transform(elmoisedlyr)
 	distances, indices = nbrs.kneighbors(lyrArr)
 	# two cases either its just in the set and:
-	if distances[0,0]<0.1:
+	if distances[0,0]<0.01:
 		resindex = indices[1]
 	else:
 		resindex = indices[0]
+	print(distances[0,0])
 	#Only lookup left:
 	lookup = np.load('./back/allyrics.npy')
 	resid = lookup[resindex, 1]
 	restrack = requests.get(url+'track.get', {'apikey':apikey, 'track_id':resid}).json()['message']['body']['track']
+	print(lyrArr)
 	print(restrack['track_name'], ' by ', restrack['artist_name'])
 
 if len(args)>4:
+	lyrs=[]
+	j=0
 	for i in range(len(args)//2):
+		try:
+			lyr, trid = find(args[i*2+1],args[i*2+2])
+		except:
+			j+=1 # to offset 
+			continue
+		
+		lyr=' '.join(lyr.replace('\n', ' ').split(' ')[:-12])
+		
+		
+		lyrs+=[lyr]
+	elmoisedlyrs = elmoiser(lyrs)
+	elmoisedlyrs = elmoisedlyrs.reshape((-1,1024))
+	lyrs = svd.transform(elmoisedlyrs)
+	# Pick the closes one:
+	listened_to = lyrs[0]
+	candidates = lyrs[1:]
+	# I'll just do it explicitely:
+	from sklearn.metrics import mean_squared_error as mse # l2 distance
+	d_min = 1000000 # since data is scaled it's safe
+	for candidate in candidates:
+		d_new = mse(listened_to, candidate)
+		if d_new<d_min:
+			d_min=d_new
+	print(d_min)
+	for candidate in candidates:
+		j+=1
+		if mse(listened_to, candidate)==d_min:
+			print(args[j*2+1], args[j*2+2])
+	
 		
